2c962808eaba8fa20f1685ccd64759770e9e787a
fabiano <fabiano.lomonaco.junior@gmail.com>
2017-11-27 22:05:35 -0200

    Final commit

--- shopping-core/src/main/java/br/edu/univas/si8/ta/shopping/rest/impl/ShoppingServiceImpl.java
+++ shopping-core/src/main/java/br/edu/univas/si8/ta/shopping/rest/impl/ShoppingServiceImpl.java
@@ -7,6 +7,7 @@ import br.edu.univas.si8.ta8.shopping.ejb.interfaces.Shopping;
 
 public class ShoppingServiceImpl implements ShoppingService {
 	
+	//Verificar - importante
 	@EJB(mappedName = "java:app/shopping-ejb-0.1-SNAPSHOT/ShoppingBean!br.edu.univas.si8.ta8.shopping.ejb.interfaces.ShoppingRemote")
 	
 	private Shopping shopping;
